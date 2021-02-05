e546587fd1545823d1dd55f0614d8b29ede609f7
Paul Walker <paul@blacksun.org.uk>
2021-02-05 17:22:14 +0000

    try to enable ssl

--- knexfile.js
+++ knexfile.js
@@ -5,11 +5,17 @@ module.exports = {
   development: {
     client: 'pg',
     connection: "postgres://postgres:postgres@localhost:5432/reviewidget_dev",
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
 
   staging: {
     client: 'pg',
     connection: "postgres://postgres:postgres@localhost:5432/reviewidget_staging",
+    ssl: {
+      rejectUnauthorized: false
+    },
     pool: {
       min: 2,
       max: 10
@@ -22,6 +28,9 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
     pool: {
       min: 2,
       max: 10
