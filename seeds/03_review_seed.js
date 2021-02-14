fd7b6baac940d822e598e032190262594187cb41
Paul Walker <paul@blacksun.org.uk>
2021-02-14 17:35:36 +0000

    db: seed files query IDs to use

--- seeds/03_review_seed.js
+++ seeds/03_review_seed.js
@@ -1,11 +1,11 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('reviews').del()
-    .then(function () {
+    .then(async function () {
       // Inserts seed entries; assuming seed site is in as 1.
+      let site_id = await knex("sites").first(["id"]).where({ domain: 'localhost' });
       return knex('reviews').insert([
-        {site_id: 1, domain: 'localhost', rating: 5, remoteIp: "127.0.0.1"}
+        {site_id: site_id.id, domain: 'localhost', rating: 5, remoteIp: "127.0.0.1"}
       ]);
     });
 };
