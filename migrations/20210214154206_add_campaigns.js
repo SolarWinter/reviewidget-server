c0e999a989b37a407862639ebc273b6cd6b4af7c
Paul Walker <paul.walker@broadcom.com>
2021-02-19 16:43:24 +0000

    db: fix campaigns migration rollback

--- migrations/20210214154206_add_campaigns.js
+++ migrations/20210214154206_add_campaigns.js
@@ -27,12 +27,32 @@ exports.up = async function(knex) {
 };
 
 exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists('campaigns');
-
-  return knex.schema.table("sites", t => {
-    t.string("reviewSiteUrl").notNullable();
-    t.string("reviewSiteName").notNullable();
-    t.integer("reviewThreshold").notNullable();
-    t.string("thankText").notNullable();
+  /* Has to be done in several phases; add the columns as nullable... */
+  await knex.schema.alterTable("sites", t => {
+    t.string("reviewSiteUrl").nullable();
+    t.string("reviewSiteName").nullable();
+    t.integer("reviewThreshold").nullable();
+    t.string("thankText").nullable();
   });
+
+  /* ...copy data from campaign columns to sites... */
+  const data = await knex.table("campaigns")
+                         .select(["site_id","reviewSiteUrl","reviewSiteName","reviewThreshold","thankText"]);
+  for (row of data) {
+    const tmp_id = row.site_id;
+    delete row.site_id;
+    console.log("Updating 'sites'", tmp_id, "with data", row);
+    await knex("sites").where({ id: tmp_id }).update(row);
+  }
+
+  /* ... then make them not nullable. */
+  await knex.schema.alterTable("sites", t => {
+    t.string("reviewSiteUrl").notNullable().alter();
+    t.string("reviewSiteName").notNullable().alter();
+    t.integer("reviewThreshold").notNullable().alter();
+    t.string("thankText").notNullable().alter();
+  });
+
+  /* Then drop the campaigns table. (Finally.) */
+  return knex.schema.dropTableIfExists('campaigns');
 };
