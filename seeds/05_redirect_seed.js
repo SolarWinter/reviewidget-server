7ebfd746a960643a8e35862fe58d3f86b45f19ba
Paul Walker <paul@blacksun.org.uk>
2021-03-16 23:00:01 +0000

    test: change seeds to make tests faster

--- seeds/05_redirect_seed.js
+++ seeds/05_redirect_seed.js
@@ -7,14 +7,26 @@ function ensureInt(i) {
   return i;
 }
 
+
 async function insertRow(db, campaignId, remoteIp) {
-  const geoIpData = await iplocate(remoteIp);
+  let geoIpData;
+  if (process.env.NODE_ENV == "test") {
+    const geoIpDataBlocks = [
+      { "ip": "216.58.212.228", "asn": "AS15169", "org": "GOOGLE", "city": "Mountain View", "country": "United States", "latitude": 37.4043, "continent": "North America", "longitude": -122.0748, "time_zone": "America/Los_Angeles", "postal_code": "94043", "subdivision": "California", "country_code": "US", "subdivision2": null },
+      { "ip": "68.183.255.165", "asn": "AS14061", "org": "DIGITALOCEAN-ASN", "city": "London", "country": "United Kingdom", "latitude": 51.5353, "continent": "Europe", "longitude": -0.6658, "time_zone": "Europe/London", "postal_code": "SL1", "subdivision": "England", "country_code": "GB", "subdivision2": null },
+      { "ip": "199.232.36.81", "asn": "AS54113", "org": "FASTLY", "city": "New York", "country": "United States", "latitude": 40.7126, "continent": "North America", "longitude": -74.0066, "time_zone": "America/New_York", "postal_code": "10118", "subdivision": "New York", "country_code": "US", "subdivision2": null }
+    ]
+    geoIpData = geoIpDataBlocks.find(rec => rec.ip == remoteIp);
+  } else {
+    geoIpData = await iplocate(remoteIp);
+  }
   return db.insert({ campaign_id: campaignId, remoteIp: remoteIp, geoIpData: geoIpData});
 }
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('redirects').del()
+  return knex('redirects')
+    .del()
     .then(async function () {
       const db = knex("redirects");
       const localhostId = await knex("sites").first(["id"]).where({ domain: 'localhost' });
